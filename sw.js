1f3836804442b830b349bc035b0d8c7e77ccb59e
trpk79yr67-beep <trpk79yr67@privaterelay.appleid.com>
2026-08-11 01:57:27 +0530

    Assets
        assets/icon192.png
        assets/icon180.png
        assets/icon512.png

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='card-bill-manager-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon-180.png','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='cbm-cred-v1';const A=['./','./index.html','./manifest.webmanifest','./assets/icon-180.png','./assets/icon-192.png','./assets/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
